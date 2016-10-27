f6b9f61fdb6a9855b4a5ba1ba67d56c09651f18c
Feraijo <Feraijo@gmail.com>
2016-10-27 14:38:08 +0500

    Added better readability of words - delay now depends on punctuation, words now also concatenates with single-placed punctuation symbol.

--- src/GoButton.java
+++ src/GoButton.java
@@ -4,26 +4,31 @@ import java.util.List;
 
 /**
  * Класс, отвечающий за кнопку и её действие
- * Created by Feraijo isPause 25.10.2016.
+ * Created by Feraijo 25.10.2016.
  */
 
 class GoButton extends JButton {
     private Reader rdr = new Reader();
     private int mainCount = 0;
     private boolean isPause = false;
-
+    private Timer timer;
     private List<String> words = rdr.getWords();
     private int wordsSize = rdr.getWords().size();
 
     GoButton(TextField textField) {
+
         setText("Play");
         ActionListener readTextAction = (e) -> {
+            timer.setDelay(rdr.getDelay());
             if (mainCount > wordsSize - 1)
                 return;
+            if (!(mainCount+1>=words.size())) //Проверка на конец читаемого файла
+                setCurrentDelay(words.get(mainCount+1));
             textField.setText(words.get(mainCount++));
         };
 
-        Timer timer = new Timer(rdr.getDelay(), readTextAction);
+        //Таймер, выполняющий действие через определённую задержку
+        timer = new Timer(rdr.getDelay(), readTextAction);
         addActionListener((e) -> {
             if (!isPause) {
                 timer.start();
@@ -35,6 +40,16 @@ class GoButton extends JButton {
                 isPause = false;
             }
         });
+    }
+
+    //Метод для динамического изменения задержки при наличии знаков препинания.
+    private void setCurrentDelay (String word){
+        if (word.isEmpty())
+            return;
+        if (word.contains(",") || word.contains(":"))
+            timer.setDelay((int)(rdr.getDelay()*1.45));
+        if (word.contains(".") ||word.contains("!") || word.contains("?") || word.contains(";"))
+            timer.setDelay((int)(rdr.getDelay()*1.8));
     }
 }
 
--- src/Reader.java
+++ src/Reader.java
@@ -1,6 +1,8 @@
 import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * Класс для чтения текстового файла
@@ -9,23 +11,23 @@ import java.util.List;
 
 public class Reader {
     private List<String> words;
-    private int speed = 250;
+    private int speed = 300;
     private File file;
+    private int count, length;
+    private String[] lines;
 
     public Reader(){
-        setFile("try.txt");
+        setFile("try.txt"); //Заглушка для выбора файла для чтения.
         try (BufferedReader reader = new BufferedReader
                 (new InputStreamReader(new FileInputStream(file), "Cp1251"))){
             words = new ArrayList<>();
             String line;
             while ((line = reader.readLine()) != null) {
-                for (String word : line.split(" ")){
-                    writeWords(word);
+                lines = line.split(" ");
+                length = lines.length;
+                for (count = 0; count < length; count++){
+                    writeWords(lines[count]);
                 }
-
-                //объединить самые маленькие слова по одной букве по два или по три в одну ячейку
-                //отдельностоящие знаки препинания туда же
-                //добавить времени задержки при наличии определённого знака препинания в слове
             }
         } catch(IOException e){
             e.printStackTrace();
@@ -40,7 +42,16 @@ public class Reader {
         return 60000/speed;
     }
 
+    //Метод для улучшения читаемости слов - длинные слова делит на короткие части,
+    // отдельностоящие знаки препинания прикрепляет к следующим за ними словам.
     private void writeWords(String word) {
+        Pattern p = Pattern.compile("\\p{Punct}+");
+        Matcher m = p.matcher(word);
+        if (m.matches()){
+            writeWords(word.concat(" ").concat(lines[count+1]));
+            count++;
+            return;
+        }
         if ((float) word.length() / 6 >= 2f) {
             words.add(word.substring(0, 7).concat("-"));
             writeWords(("-").concat(word.substring(7)));
